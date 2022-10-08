64b43859a4505abe48c35f6db76bed28d59d100c
Paige <prhoward97@gmail.com>
2022-10-08 08:30:14 -0400

    commit tree 74ec1932920e916d2130a930922082ba523e08c4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+// will loop through the years and find the first object whose cond returns true based on callback
+function superbowlWin(record){
+  const win = record.find(({result}) => result === "W");
+  if (win){
+    return win.year;
+  }
+  else {
+    return undefined
+  }
+}
+   
+    
+  
+  
+//will find the first year
